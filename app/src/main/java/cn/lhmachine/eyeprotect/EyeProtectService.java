70e56d36281c9ccf16d4ac87787a8bcbfb15151a
lhmachine <lhmachine@qq.com>
2018-10-26 14:57:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/lhmachine/eyeprotect/EyeProtectService.java
@@ -0,0 +1,102 @@
+package cn.lhmachine.eyeprotect;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * @author lhmachine
+ * 声明一个服务用来增加一个护眼悬浮罩，覆盖在整个view上
+ */
+
+public class EyeProtectService extends Service {
+
+    private FrameLayout mView;
+    private static final int NOTIFY_ID = 1;
+
+    public EyeProtectService() {
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+        startProtect(pref.getInt("dim", 50));
+        showNotification();
+    }
+
+    //开启护眼
+    private void startProtect(int dim){
+        //新建一个FramLayout，并设置背景颜色
+        mView = new FrameLayout(this);
+        mView.setBackgroundColor(Color.argb(dim,199,237,204));
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            params.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        //设置效果透明，否则全黑
+        params.format=PixelFormat.RGBA_8888;
+        //最后两个标志用来覆盖状态栏
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        params.gravity = Gravity.RIGHT | Gravity.TOP;
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        wm.addView(mView, params);
+    }
+
+    //通知栏显示信息
+    private void showNotification(){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle("Eye Protect").setContentText("护眼模式已开启");
+        //创建通知背点击时触发的Intent
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        //创建任务栈Builder
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotifyMgr = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        //构建通知
+        final Notification notification = mBuilder.build();
+        //显示通知
+        mNotifyMgr.notify(NOTIFY_ID, notification);
+        //启动为前台服务
+        startForeground(NOTIFY_ID, notification);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy(){
+        if (mView != null){
+            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(mView);
+            mView = null;
+        }
+    }
+}
---
+++ app/src/main/java/cn/lhmachine/eyeprotect/MainActivity.java
@@ -0,0 +1,217 @@
+package cn.lhmachine.eyeprotect;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.Permission;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean open_symbol = false;
+    private ImageButton bt;
+    private Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (ImageButton) findViewById(R.id.bt_eye);
+
+        //检查权限
+        checkPermission();
+
+        //护眼模式开启或关闭
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //判断护眼模式是否开启
+                if (open_symbol){
+                    //关闭护眼
+                    open_symbol = false;
+                    bt.setBackgroundResource(R.drawable.ic_eye_close);
+                    stopService(new Intent(getApplicationContext(), EyeProtectService.class));
+                }else{
+                    //开启护眼
+                    open_symbol = true;
+                    bt.setBackgroundResource(R.drawable.ic_eye_open);
+                    startService(new Intent(getApplicationContext(), EyeProtectService.class));
+                }
+                if (isServiceWork(getApplicationContext(), "cn.lhmachine.eyeprotect.TimeService")){
+                    stopService(new Intent(getApplicationContext(), TimeService.class));
+                    Toast.makeText(MainActivity.this, "定时功能已关闭", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //个人设置
+        findViewById(R.id.setting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SettingActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        //检查服务是否在运行
+        SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+        if(pref.getBoolean("onTimeOpen", false)){
+            if (!isServiceWork(getApplicationContext(), "cn.lhmachine.eyeprotect.TimeService")){
+                startService(new Intent(getApplicationContext(),TimeService.class));
+            }
+        }
+
+        if (isServiceWork(getApplicationContext(), "cn.lhmachine.eyeprotect.EyeProtectService")){
+            open_symbol = true;
+            bt.setImageResource(R.drawable.ic_eye_open);
+        }else{
+            open_symbol = false;
+            bt.setImageResource(R.drawable.ic_eye_close);
+        }
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+            if (isServiceWork(getApplicationContext(), "cn.lhmachine.eyeprotect.EyeProtectService")){
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+            }
+        }, 0, 500);
+    }
+
+    final Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message message){
+            if (message.what == 1){
+                open_symbol = true;
+                bt.setImageResource(R.drawable.ic_eye_open);
+            }else{
+                open_symbol = false;
+                bt.setImageResource(R.drawable.ic_eye_close);
+            }
+        }
+    };
+
+    /**
+     * 权限检查
+     */
+    private void checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            PackageManager pm = getPackageManager();
+            if (!Settings.System.canWrite(this) || !Settings.canDrawOverlays(this) ){
+                //未被授予权限
+                showDialogTipUserRequestPermission();
+            }
+        }
+    }
+
+    /**
+     * 请求权限弹出框
+     */
+    private void showDialogTipUserRequestPermission() {
+        new AlertDialog.Builder(this)
+                .setTitle("权限不可用")
+                .setMessage("由于本app需要获取您的悬浮框和系统设置授权；\n否则，您将无法使用本App")
+                .setPositiveButton("立即开启", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startRequestPermission();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).setCancelable(false).show();
+    }
+
+    /**
+     * 开始申请权限
+     */
+    private void startRequestPermission(){
+        getOverlayPermission();
+        getSettingPermission();
+    }
+
+    /**
+     * 申请悬浮窗权限
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void getOverlayPermission() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, 0);
+    }
+
+    /**
+     * 申请系统设置权限
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void getSettingPermission(){
+        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivityForResult(intent, 0);
+    }
+
+    /**
+     * 判断权限是否正在运行
+     * @param mContext 全局环境
+     * @param serviceName 服务名
+     * @return 布尔类型变量:true-服务正在运行;false-服务没有运行
+     */
+    public boolean isServiceWork(Context mContext, String serviceName) {
+        boolean isWork = false;
+        ActivityManager myAM = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> myList = myAM.getRunningServices(40);
+        if (myList.size() <= 0) {
+            return false;
+        }
+        for (int i = 0; i < myList.size(); i++) {
+            String mName = myList.get(i).service.getClassName();
+            if (mName.equals(serviceName)) {
+                isWork = true;
+                break;
+            }
+        }
+        return isWork;
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        timer.cancel();
+    }
+}
